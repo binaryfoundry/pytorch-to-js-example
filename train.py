936a331428323140d062d3edfba0dcabe0410818
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2019-09-04 17:43:23 +0100

    Convert Test and Training Data to PyTorch Datasets

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
 
 class Model(nn.Module):
     def __init__(self):
@@ -30,6 +32,18 @@ class Model(nn.Module):
         x = self.layer3(x)
         return x
 
+class Dataset(Dataset):
+    def __init__(self, in_data, out_data):
+        self.length = in_data.shape[0]
+        self.in_data = torch.from_numpy(in_data).type(torch.FloatTensor)
+        self.out_data = torch.from_numpy(out_data).type(torch.FloatTensor)
+
+    def __getitem__(self, index):
+        return self.in_data[index], self.out_data[index]
+
+    def __len__(self):
+        return self.length
+
 # random value 0 < x < 2Pi
 def tau_rand(x):
     return random.uniform(0, 2 * math.pi)
@@ -41,6 +55,9 @@ def lmap(func, data):
 # No. of data items for training and for testing
 train_data_size = 100000
 test_data_size = 8000
+
+# No. of samples used for back propagation gradient
+batch_size = 16
 
 # create arrays of random values 0 < x < 2Pi
 in_data_x_train = lmap(tau_rand, [0] * train_data_size)
@@ -60,3 +77,10 @@ in_data_test = np.row_stack(in_data_x_test)
 
 out_data_train = np.column_stack((out_data_u_train, out_data_v_train))
 out_data_test = np.column_stack((out_data_u_test, out_data_v_test))
+
+# numpy tables to pytorch Datasets
+data_train = Dataset(in_data=in_data_train, out_data=out_data_train)
+data_test = Dataset(in_data=in_data_test, out_data=out_data_test)
+
+data_loader_train = DataLoader(dataset=data_train, batch_size=batch_size, shuffle=True)
+data_loader_test = DataLoader(dataset=data_test, batch_size=len(data_test), shuffle=False)
