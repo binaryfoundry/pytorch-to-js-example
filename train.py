81454ff0da0f4bcb45aab62066075a5fb50b5076
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2019-09-04 17:25:20 +0100

    Define Pytorch Model

--- train.py
+++ train.py
@@ -2,6 +2,34 @@ import random
 import math
 import numpy as np
 
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+        in_count = 1  # only one variable as input 'x'.
+        hidden_1 = 64 # hidden layers have 64 nodes each.
+        hidden_2 = 64
+        hidden_3 = 64
+        out_count = 2 # two output vars 'u' and 'v'.
+
+        self.layer0 = nn.Linear(in_count, hidden_1)
+        self.layer1 = nn.Linear(hidden_1, hidden_2)
+        self.layer2 = nn.Linear(hidden_2, hidden_3)
+        self.layer3 = nn.Linear(hidden_3, out_count)
+
+    def forward(self, x):
+        x = self.layer0(x)
+        x = torch.tanh(x)
+        x = self.layer1(x)
+        x = torch.tanh(x)
+        x = self.layer2(x)
+        x = torch.tanh(x)
+        x = self.layer3(x)
+        return x
+
 # random value 0 < x < 2Pi
 def tau_rand(x):
     return random.uniform(0, 2 * math.pi)
